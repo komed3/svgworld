bb2099df3279b4d17f19f15e8c8f948a61d5cb54
komed3 <pk97@outlook.de>
2024-01-04 00:46:37 +0100

    first steps

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+export default class SVGWorld {
+
+    options = {};
+
+    container;
+    chart;
+
+    constructor ( container, data = {} ) {
+
+        if( !container || container.length == 0 ) {
+
+            throw 'SVGWorld error: container does not exists';
+
+        }
+
+        this.container = container;
+
+        this.options = {
+            ...this.options,
+            ...data
+        };
+
+    };
+
+    setSize () {};
+
+    update () {};
+
+};
