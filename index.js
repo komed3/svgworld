62099d35d3735d6be0973ac0d3b8419dd9a9355c
komed3 <pk97@outlook.de>
2024-01-05 00:40:27 +0100

    load map

--- index.js
+++ index.js
@@ -40,6 +40,63 @@ export default class SVGWorld {
         this.container = container;
         this.options = options;
 
+        this.#loadMap();
+
+    };
+
+    #loadMap () {
+
+        let map = this.options.map || null;
+
+        /**
+         * if map is not defined or contains no paths
+         * throw error
+         */
+        if( typeof map != 'object' || !map.paths || typeof map.paths != 'object' ) {
+
+            throw new Element( '[SVGWorld ERR] map is not defined or contains no paths' );
+
+        }
+
+        /**
+         * create SVG map
+         */
+
+        let svgns = 'http://www.w3.org/2000/svg';
+
+        let svg = document.createElementNS( svgns, 'svg' );
+
+        svg.classList.add( 'svgworld-plane' );
+        svg.setAttribute( 'viewBox', map.viewBox || '0 0 100 100' );
+
+        this.container.appendChild( svg );
+
+        /**
+         * create SVG series group
+         */
+
+        let group = document.createElementNS( svgns, 'g' );
+
+        group.classList.add( 'svgworld-series' );
+
+        svg.appendChild( group );
+
+        /**
+         * create SVG paths
+         */
+
+        Object.values( map.paths ).forEach( ( p ) => {
+
+            let path = document.createElementNS( svgns, 'path' );
+
+            path.classList.add( 'svgworld-path' );
+            path.setAttribute( 'map-id', p.id );
+            path.setAttribute( 'd', p.path );
+
+            group.appendChild( path );
+
+        } );
+
     };
 
 };
