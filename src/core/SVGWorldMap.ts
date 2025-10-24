cfae17c007d43065488afe6af9c51e6a3eb53baa
komed3 <pk97@outlook.de>
2025-10-24 23:38:26 +0200

    implement the hook system

--- src/core/SVGWorldMap.ts
+++ src/core/SVGWorldMap.ts
@@ -1,9 +1,10 @@
-import { MapOptions } from '../types';
+import { HookCallback, Hooks, MapEvent, MapOptions } from '../types';
 
 export class SVGWorldMap {
 
     private container: HTMLElement;
     private svg: SVGSVGElement;
+    private hooks: Hooks = {};
 
     constructor ( options: MapOptions ) {
 
@@ -16,6 +17,34 @@ export class SVGWorldMap {
         this.svg.setAttribute( 'height', options.height?.toString() || '100%' );
         this.svg.setAttribute( 'viewBox', '0 0 1000 500' );
         this.container.appendChild( this.svg );
+
+    }
+
+    // Hook system
+
+    public addHook ( event: string, callback: HookCallback ) : void {
+
+        if ( ! this.hooks[ event ] ) this.hooks[ event ] = [];
+        this.hooks[ event ].push( callback );
+
+    }
+
+    public removeHook ( event: string, callback: HookCallback ) : void {
+
+        if ( this.hooks[ event ] ) this.hooks[ event ] = this.hooks[ event ].filter(
+            cb => cb !== callback
+        );
+
+    }
+
+    private triggerHook ( event: string, data?: any ) : void {
+
+        if ( this.hooks[ event ] ) {
+
+            const mapEvent: MapEvent = { type: event, target: this, data };
+            this.hooks[ event ].forEach( callback => callback( mapEvent ) );
+
+        }
 
     }
 
