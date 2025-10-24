e58699152ff018083a2976665c41c3a2f8c3172c
komed3 <pk97@outlook.de>
2025-10-24 23:32:42 +0200

    svg world map constructor

--- src/core/SVGWorldMap.ts
+++ src/core/SVGWorldMap.ts
@@ -1,1 +1,22 @@
-export class SVGWorldMap {}
+import { MapOptions } from '../types';
+
+export class SVGWorldMap {
+
+    private container: HTMLElement;
+    private svg: SVGSVGElement;
+
+    constructor ( options: MapOptions ) {
+
+        this.container = typeof options.container === 'string'
+            ? document.getElementById( options.container ) || document.body
+            : options.container;
+
+        this.svg = document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' );
+        this.svg.setAttribute( 'width', options.width?.toString() || '100%' );
+        this.svg.setAttribute( 'height', options.height?.toString() || '100%' );
+        this.svg.setAttribute( 'viewBox', '0 0 1000 500' );
+        this.container.appendChild( this.svg );
+
+    }
+
+}
