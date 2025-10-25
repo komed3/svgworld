1f212ef075564d08807f5cb1ab6be1dc3b9346dc
komed3 <pk97@outlook.de>
2025-10-25 09:33:39 +0200

    Create ThemeManager.ts

---
+++ src/core/ThemeManager.ts
@@ -0,0 +1,9 @@
+import { Theme } from '../types';
+import { SVGWorldMap } from './SVGWorldMap';
+
+export class ThemeManager {
+
+    private map: SVGWorldMap;
+    private currentTheme: Theme;
+
+}
